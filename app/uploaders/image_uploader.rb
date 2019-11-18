eca26846717bc8e38d3eca45bdcc2fea2f46f9f5
takatakatakashi <iverson3kobe0824@gmil.com>
2019-11-18 12:47:53 +0900

    グループにメッセージを表示

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,8 @@
-include CarrierWave::MiniMagick
-  process resize_to_fit: [800, 800]class ImageUploader < CarrierWave::Uploader::Base
+class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
+  process resize_to_fit: [800, 800]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
