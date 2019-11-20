4884d5b9813f4f0ebd5936a4566abb7ec0d29f48
takatakatakashi <iverson3kobe0824@gmil.com>
2019-11-20 19:37:57 +0900

    インクリメンタルサーチ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
-  
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    respond_to do |format|
   def edit
   end
-
   def update
     if current_user.update(user_params)
       redirect_to root_path
@@ -10,9 +12,7 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-
   private
-
   def user_params
     params.require(:user).permit(:name, :email)
   end
