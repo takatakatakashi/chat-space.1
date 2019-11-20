4ca4436e11df8b6f4f1cf164909d179b975651d1
takatakatakashi <iverson3kobe0824@gmil.com>
2019-11-20 10:16:46 +0900

    非同期

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,26 +39,25 @@ $(function(){
      return html;
    };
  }
-$('.js-form').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-    $('form')[0].reset();
-  })
-})
-  .fail(function(){
-    alert('error');
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+      }).done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        $('form')[0].reset();
+      }).fail(function(){
+        alert('error');
+      });
+      return false;
   });
-  return false;
 });
